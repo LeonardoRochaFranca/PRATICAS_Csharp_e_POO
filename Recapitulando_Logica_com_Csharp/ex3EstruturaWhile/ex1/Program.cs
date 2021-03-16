9859c336bbe6e0087e5e51535c1e98af3cd8b023
ahcoRodranoeL <franca.leonardorocha@gmail.com>
2021-03-15 21:37:09 -0300

    commit

---
+++ Recapitulando_Logica_com_Csharp/ex3EstruturaWhile/ex1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+//Escreva um programa que repita a leitura de uma senha até que ela seja válida. Para cada leitura de senha incorreta informada, escrever a mensagem "Senha Invalida". Quando a senha for informada corretamente deve ser impressa a mensagem "Acesso Permitido" e o algoritmo encerrado. Considere que a senha correta é o valor 2002.
+            System.Console.Write("Digite a senha: ");
+            int senha = int.Parse(Console.ReadLine());
+
+            while(senha!= 2002){
+                System.Console.WriteLine("Senha Invalida");
+                System.Console.Write("Digite a senha novamente: ");
+                senha = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Acesso Permitido");
+        }
+    }
+}
---
+++ Recapitulando_Logica_com_Csharp/ex3EstruturaWhile/ex2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+//Escreva um programa para ler as coordenadas (X,Y) de uma quantidade indeterminada de pontos no sistema cartesiano. Para cada ponto escrever o quadrante a que ele pertence. O algoritmo será encerrado quando pelomenos uma de duas coordenadas for NULA (nesta situação sem escrever mensagem alguma).
+            string [] coordenadas = Console.ReadLine().Split(' ');
+            int x = int.Parse(coordenadas[0]);
+            int y = int.Parse(coordenadas[1]);
+            while( x!= 0 || x!=0){
+                if( x > 0 && y > 0){
+                    System.Console.WriteLine("primeiro");
+                }else if(x < 0 && y > 0){
+                    System.Console.WriteLine("segundo");
+                }else if( x < 0 && y < 0){
+                    System.Console.WriteLine("terceiro");
+                }else{
+                    System.Console.WriteLine("quarto");
+                }
+                coordenadas = Console.ReadLine().Split(' ');
+                x = int.Parse(coordenadas[0]);
+                y = int.Parse(coordenadas[1]);
+            }
+            
+        }
+    }
+}
